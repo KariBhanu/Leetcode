e904ae348bc5310b725bda17cb22f659d70570b5
Bhanu <bhanuchoudary1999@gmail.com>
2024-02-09 16:51:39 +0530

    Time: 47 ms (86.01%) | Memory: 48.8 MB (23.47%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    const res = haystack.indexOf(needle)
+    return res;
+};
