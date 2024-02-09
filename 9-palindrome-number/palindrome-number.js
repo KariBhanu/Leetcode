907f81341a003ba54b735c1f35b0761968931768
Bhanu <bhanuchoudary1999@gmail.com>
2024-02-09 16:38:52 +0530

    Time: 158 ms (23.95%) | Memory: 58.2 MB (18.84%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -3,6 +3,6 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    let temp = (String(x).split("").reverse().join(""))
+    let temp = String(x).split("").reverse().join("")
     return temp == String(x)
 };
