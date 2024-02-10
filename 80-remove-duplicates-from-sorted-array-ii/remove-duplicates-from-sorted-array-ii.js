3313e693a12464fb136360714a41115944012c5b
Bhanu <bhanuchoudary1999@gmail.com>
2024-02-10 17:25:09 +0530

    Time: 70 ms (24.09%) | Memory: 51 MB (35.11%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let i=0;j=2;
+    let count = 0 ;
+    while(j<nums.length){
+        if(nums[i]===nums[j]){
+            nums.splice(j,1)
+            //count++;
+            continue;
+        }
+        i++;
+        j++;
+    }
+
+    return nums.length
+};
