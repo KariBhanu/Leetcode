ac72952950945164aace3d9cbebd158587e4877b
Bhanu <bhanuchoudary1999@gmail.com>
2025-02-21 12:54:06 +0530

    Time: 1 ms (96.34%) | Memory: 62.1 MB (5.07%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.js
+++ 11-container-with-most-water/container-with-most-water.js
@@ -3,22 +3,39 @@
  * @return {number}
  */
 var maxArea = function(height) {
-    let res = 0;
-    let i = 0,j=height.length - 1;
+    let i = 0;
+    let j = height.length - 1 ;
+    let maxArealength = 0;
+    // for(let i = 0;i<height.length;i++){
+    //     for(let j = height.length-1;j>=0;j--){
+    //         let bredth = j - i; // 8 - 0 = 8 // 8 - 1 = 7
+    //         if(i>j){
+    //             continue;
+    //         }
+    //         let area = 0;
+    //         if(height[i]<=height[j]){
+    //             area = height[i] * bredth;
+    //         } else if(height[i]>height[j]){
+    //             area = height[j] * bredth;
+    //         }
+            
+    //         if(area > maxArealength){
+    //             maxArealength = area
+    //         }
+    //     }
+    // }
     while(i<j){
-        if(height[i]<height[j]){
-            let area = height[i] * (j-i);
-            if(area > res){
-                res = area
-            }
+        let area = 0;
+        if(height[i] <= height[j]){
+            area = height[i] * (j-i);
             i++;
-        } else if(height[i]>=height[j]){
-            let area = height[j] * (j-i);
-            if(area > res){
-                res = area
-            }
+        } else if(height[i] > height[j]){
+            area = height[j] * (j-i);
             j--;
         }
+        if(area > maxArealength){
+            maxArealength = area
+        }
     }
-    return res
+    return maxArealength;
 };
