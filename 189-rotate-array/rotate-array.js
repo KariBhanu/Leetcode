c210157d2496868656a2a18827ae7ad57c0d229b
Bhanu <bhanuchoudary1999@gmail.com>
2025-04-25 14:56:08 +0530

    Time: 0 ms (100.00%) | Memory: 66.6 MB (20.62%) - LeetSync

--- 189-rotate-array/rotate-array.js
+++ 189-rotate-array/rotate-array.js
@@ -4,19 +4,17 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    // var l = nums.length-1;
-    // for(let i = 0;i<k;i++){
-    //     nums.splice(0,0,nums[l]);
-    // }
 
-    // nums.splice(nums.length - k,k)
-      k %= nums.length;
-    
+    //k will be the middle 
+    k %= nums.length;
+
     // Reverse the entire array
     reverse(nums, 0, nums.length - 1);
+
     
     // Reverse the first k elements
     reverse(nums, 0, k - 1);
+
     
     // Reverse the rest of the elements
     reverse(nums, k, nums.length - 1);
