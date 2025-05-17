3cdbb3f4702a83d73a5ac51dd3d31ed60901d69f
Bhanu <bhanuchoudary1999@gmail.com>
2025-05-17 13:50:41 +0530

    Time: 6 ms (26.43%) | Memory: 59.1 MB (54.63%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
@@ -4,13 +4,6 @@
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
-    // nums1 = [...new Set(nums1)]
-    // nums2 = [...new Set(nums2)]
-    // for(let i = 0;i<nums2.length;i++){
-    //     if(!nums1.includes(nums2[1])){
-    //         nums1.push(nums2[i])
-    //     }
-    // }
     nums1 = [...nums1,...nums2].sort((a,b)=>a-b) 
     if(nums1.length%2 === 0){
         let i = nums1.length / 2;
