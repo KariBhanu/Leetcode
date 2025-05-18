8d69004fa7bee8e19d8c23f1c8570e368d53c6cc
Bhanu <bhanuchoudary1999@gmail.com>
2025-05-18 16:24:49 +0530

    Time: 0 ms (100.00%) | Memory: 53.5 MB (69.68%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    // let left = 0;
+    // let right = nums.length - 1;
+
+    // while(left < right){
+    //     if(nums[left] > target && nums)
+    // }
+
+    return nums.indexOf(target)
+};
